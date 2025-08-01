c7eb9f7c8f1cf915fca7c3c267d6f5708f55bff2
Arman Arif <me@armanarif.com>
2025-08-01 09:15:42 +0600

    database model

---
+++ app/Models/Task.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use App\Enums\TaskStatus;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'description',
+        'status',
+        'due_date',
+    ];
+
+    protected $casts = [
+        'due_date' => 'datetime',
+        'status' => TaskStatus::class,
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
